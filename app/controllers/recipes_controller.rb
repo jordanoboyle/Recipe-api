22e39d7f4ec8d82d60b33858ef5558297b57b863
jordanoboyle <jmcmcode@gmail.com>
2024-05-20 17:31:45 -0600

    create hard code test

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,13 @@
 class RecipesController < ApplicationController
 
   def create
-    # @recipe = Recipe.new(
-    #   title: 
-    # )
-    render json: {mess: "hellow there"}
+    @recipe = Recipe.new(
+      title: "Test recipe",
+      ingreds: "Test ingred1",
+      directions: "Test direction",
+      prep_time: 20,
+      )
+    @recipe.save
+    render template: "recipes/show"
   end
 end
--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -4,4 +4,9 @@ class RecipesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "create" do
+    assert_difference "Recipe.count", 1 do
+      post "/recipes.json", params: { title: "test product", ingreds: "food", directions: "do stuff", prep_time: 2  }
+    end
+  end
 end
